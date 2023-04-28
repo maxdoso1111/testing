324a6de1a89f8b90b84578df773ab2d4c8ec303a
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-04-28 06:14:52 +0200

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,5 +1,54 @@
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
+interface PostProps {
+	post: any;
+	host: string;
+	path: string;
+}
+
+const Post: React.FC<PostProps> = (props) => {
+	const { post, host, path } = props;
+
+	// to remove tags from excerpt
+	const removeTags = (str: string) => {
+		if (str === null || str === '') return '';
+		else str = str.toString();
+		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+	};
+
+	return (
+		<>
+			<Head>
+				<meta property="og:title" content={post.title} />
+				<link rel="canonical" href={`https://${host}/${path}`} />
+				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:url" content={`https://${host}/${path}`} />
+				<meta property="og:type" content="article" />
+				<meta property="og:locale" content="en_US" />
+				<meta property="og:site_name" content={host.split('.')[0]} />
+				<meta property="article:published_time" content={post.dateGmt} />
+				<meta property="article:modified_time" content={post.modifiedGmt} />
+				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta
+					property="og:image:alt"
+					content={post.featuredImage.node.altText || post.title}
+				/>
+				<title>{post.title}</title>
+			</Head>
+			<div className="post-container">
+				<h1>{post.title}</h1>
+				<img
+					src={post.featuredImage.node.sourceUrl}
+					alt={post.featuredImage.node.altText || post.title}
+				/>
+				<article dangerouslySetInnerHTML={{ __html: post.content }} />
+			</div>
+		</>
+	);
+};
+
+export default Post;
+
 
 export default function Meta() {
   return (
