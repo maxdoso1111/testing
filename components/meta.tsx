5ce9027cd0192ec6d2db2caabc5f43e9a024a8dc
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-04-28 15:31:17 +0200

    Update meta.tsx

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,92 +1,41 @@
-import React from 'react';
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
-interface PostProps {
-	post: any;
-	host: string;
-	path: string;
-}
-
-const Post: React.FC<PostProps> = (props) => {
-	const { post, host, path } = props;
-
-	// to remove tags from excerpt
-	const removeTags = (str: string) => {
-		if (str === null || str === '') return '';
-		else str = str.toString();
-		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
-	};
-
-	return (
-		<>
-			<Head>
-				<meta property="og:title" content={post.title} />
-				<link rel="canonical" href={`https://${host}/${path}`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
-				<meta
-					property="og:image:alt"
-					content={post.featuredImage.node.altText || post.title}
-				/>
-				<title>{post.title}</title>
-			</Head>
-			<div className="post-container">
-				<h1>{post.title}</h1>
-				<img
-					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage.node.altText || post.title}
-				/>
-				<article dangerouslySetInnerHTML={{ __html: post.content }} />
-			</div>
-		</>
-	);
-};
-
-export default Post;
-
 
 export default function Meta() {
-	return (
-		<Head>
-			<link
-				rel="apple-touch-icon"
-				sizes="180x180"
-				href="/favicon/apple-touch-icon.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="32x32"
-				href="/favicon/favicon-32x32.png"
-			/>
-			<link
-				rel="icon"
-				type="image/png"
-				sizes="16x16"
-				href="/favicon/favicon-16x16.png"
-			/>
-			<link rel="manifest" href="/favicon/site.webmanifest" />
-			<link
-				rel="mask-icon"
-				href="/favicon/safari-pinned-tab.svg"
-				color="#000000"
-			/>
-			<link rel="shortcut icon" href="/favicon/favicon.ico" />
-			<meta name="msapplication-TileColor" content="#000000" />
-			<meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-			<meta name="theme-color" content="#000" />
-			<link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-			<meta
-				name="description"
-				content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
-			/>
-			
-		</Head>
-	)
+  return (
+    <Head>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#000000"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#000" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta
+        name="description"
+        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+      />
+    </Head>
+  )
 }
