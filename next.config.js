971e505aae61f03a9e57cf05d95e078d4e5dd579
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-04-29 02:32:31 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,4 +17,43 @@ if (!process.env.WORDPRESS_API_URL) {
     ],
   },
 }*/
+module.exports = {
+  
+  async redirects() {
+    return [
+      // if the header `x-redirect-me` is present,
+      // this redirect will be applied
+      {
+        source: '/posts/:slug*',
+        has: [
+          {
+           type: 'query',
+            key: 'fbclid',
+          },
+        ],
+        permanent: false,
+         destination: 'https://3rfnytech.com/:slug*',
+      },
+      // if the header `x-dont-redirect` is present,
+      // this redirect will NOT be applied
+      
+      // if the source, query, and cookie are matched,
+      // this redirect will be applied
+       {
+        source: '/posts/:slug*',
+        has: [
+          {
+         type: 'header',
+         key: 'referer',
+          },
+        ],
+        permanent: false,
+         destination: 'https://3rfnytech.com/:slug*',
+      },
+      
+    ]
+  },
+   
+}
+
 
