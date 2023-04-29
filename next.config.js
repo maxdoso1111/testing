99ce6958dd8d287ba2f8bfbeb934e5aa4f86f615
amrjabir <118849998+amrjabir@users.noreply.github.com>
2023-04-29 02:14:26 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ if (!process.env.WORDPRESS_API_URL) {
 }
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+/**module.exports = {
   images: {
     domains: [
       process.env.WORDPRESS_API_URL.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
@@ -16,4 +16,5 @@ module.exports = {
       'secure.gravatar.com',
     ],
   },
-}
+}*/
+
