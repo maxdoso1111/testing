3115634b47de897e3708bdf59837dea87477540c
maxdoso1111 <127878832+maxdoso1111@users.noreply.github.com>
2023-06-26 18:24:24 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@
 import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import Container from '../components/container'
-import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
 import Layout from '../components/layout'
 
