9270324f191a084368c0e75bf16d1b40ae155945
maxdoso1111 <127878832+maxdoso1111@users.noreply.github.com>
2023-06-26 18:30:27 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,20 +3,8 @@ import Head from 'next/head'
 import { GetStaticProps } from 'next'
 import Container from '../components/container'
 import { CMS_NAME } from '../lib/constants'
-import Layout from '../components/layout'
 
 export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const morePosts = edges.slice(1)
-
-  return (
-    
-      <Layout preview={preview}>
-      <Container>
-       
-      </Container>
-    </Layout>
-  )
 }
 export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   const allPosts = await getAllPostsForHome(preview)
