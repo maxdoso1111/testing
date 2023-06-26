2e3a55a0686f80641c10e26c9c64aca535f528ee
maxdoso1111 <127878832+maxdoso1111@users.noreply.github.com>
2023-06-26 18:26:42 +0300

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,14 +1,13 @@
 import Alert from './alert'
-import Footer from './footer'
 
 export default function Layout({ preview, children }) {
   return (
     <>
-      <div className="min-h-screen">
+     
         <Alert preview={preview} />
         <main>{children}</main>
       </div>
-      <Footer />
+      
     </>
   )
 }
