d1625e0686a3c0af2f6fdb345c8e8f59de32f3c3
maxdoso1111 <127878832+maxdoso1111@users.noreply.github.com>
2023-06-26 17:35:18 +0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,30 +3,14 @@ import { GetStaticProps } from 'next'
 import Container from '../components/container'
 import { getAllPostsForHome } from '../lib/api'
 import { CMS_NAME } from '../lib/constants'
+import Layout from '../components/layout'
 
 export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node
   const morePosts = edges.slice(1)
 
   return (
-    <Layout preview={preview}>
-      <Head>
-        <title>{`Next.js Blog Example with ${CMS_NAME}`}</title>
-      </Head>
-      <Container>
-        <Intro />
-        {heroPost && (
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.featuredImage}
-            date={heroPost.date}
-            author={heroPost.author}
-            slug={heroPost.slug}
-            excerpt={heroPost.excerpt}
-        />
-        )}
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-      </Container>
+    <Layout>
     </Layout>
   )
 }
